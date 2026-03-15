a904cbe190929d3db9e356c73807dee82138ca2c
gis-14292476 <xiaoyang.li-6@student.manchester.ac.uk>
2026-03-15 21:04:59 +0000

    0.2 Complete dataset creation
    Construct the dataset according to the idea of ​​presence points and background points.

--- code4A1.R
+++ code4A1.R
@@ -71,9 +71,9 @@ reclass_binary <- function(lcm, rule){
   return(result)
 }
 
-#### 1 data processing ####
+#### ----------------- 1 data processing ----------------- ####
 
-# 1.1 read data
+# 1.1 read data ----
 ## Melesmeles spatial point data （British National Grid）----
 
 Melesmeles = read.csv("./data/Melesmeles.csv")
@@ -89,6 +89,7 @@ Melesmeles_latlong = data.frame( x = Melesmeles$Longitude, y = Melesmeles$Latitu
 Melesmeles_sp = st_as_sf( Melesmeles_latlong, coords = c("x", "y"), crs = "epsg:4326")
 
 
+
 ## England shapefile ----
 england = st_read('./data/England.shp')
 
@@ -101,6 +102,8 @@ Melesmeles_sp = st_transform(Melesmeles_sp, crs(LCM))
 
 # 1.2 data processing
 
+
+# 1.2 data processing ----
 ## LCM raster ----
 LCM=crop(LCM,st_buffer(england, dist= 1000))
 LCM=aggregate(LCM$LCMUK_1,fact=4,fun="modal")
@@ -110,10 +113,9 @@ LCM=aggregate(LCM$LCMUK_1,fact=4,fun="modal")
 MelesmelesFin=Melesmeles_sp[england,]
 LCM=crop(LCM,england,mask=TRUE)
 
-
 ## Calculate environment variables ----
 reclassLeaf  = c(0,1,rep(0,20))
-reclassUrban = c(rep(0,19),1,1)
+reclassUrban = c(rep(0,20),1,1) ## difference 19
 
 broadleaf = reclass_binary(LCM, reclassLeaf)
 urban = reclass_binary(LCM, reclassUrban)
@@ -124,7 +126,75 @@ lcm_urban_2300 = calc_prop_focal(urban, 2300)
 # add why choose 1800/2300
 
 # 
-#### draw picture ####
+
+
+#### ----------------- 2 create dataset -----------------  ####
+
+# 2.1 add all environment variables ----
+allEnv=c(lcm_wood_1800,lcm_urban_2300)
+names(allEnv)=c("broadleaf","urban")
+
+
+
+# 2.2 ‘presence’ and ‘background’ -----
+## create the background points----
+set.seed(11)
+
+back = spatSample(
+  allEnv,size=2000,
+  as.points=TRUE,
+  method="random",
+  na.rm=TRUE) 
+# add why choose 2000
+
+back=back[!is.na(back$broadleaf),]
+
+back=st_as_sf(back,crs="EPSG:27700")
+
+
+## organize ‘presence’ and ‘background’----
+
+# extract env var for presence point (MelesmelesFin)
+eP=terra::extract(allEnv,MelesmelesFin)
+# env var + presence point 
+Pres.cov=st_as_sf(cbind(eP,MelesmelesFin))
+
+
+## add text for differencr point ----
+
+# add text for presence point (MelesmelesFin)
+Pres.cov$Pres=1
+# delet ID
+Pres.cov=Pres.cov[,-1]
+
+# add text for background point
+Back.cov=st_as_sf(data.frame(back,Pres=0))
+
+
+
+# 2.3 merging data
+## merging point coordinates ----
+# get coordinate
+coordsPres=st_coordinates(Pres.cov)
+coordsBack=st_coordinates(back)
+
+# merging point coordinates
+coords=data.frame(rbind(coordsPres,coordsBack))
+colnames(coords)=c("x","y")
+
+all.cov=rbind(Pres.cov,Back.cov)
+all.cov=cbind(all.cov,coords)
+
+
+## data cleaning ----
+all.cov=na.omit(all.cov)
+all.cov=st_drop_geometry(all.cov)
+
+
+
+
+
+####-----------------  draw picture ----------------- ####
 
 # figure 1 data show
 plot(st_geometry(MelesmelesFin), add=TRUE, col="red")
